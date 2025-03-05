4893c66a72bdf4efc89e4b428a3f4db2e86df965
plyb <kobyjlewis@gmail.com>
2025-03-05 18:51:06 +0000

    abstract creation of unary primops

--- src/primops.ts
+++ src/primops.ts
@@ -1,18 +1,20 @@
 import ts from 'typescript';
 import { AbstractResult, objectResult, resultBind } from './abstract-results';
-import { numberValue, primopValue } from './abstract-values';
+import { AbstractValue, LatticeKey, numberValue, primopValue } from './abstract-values';
 
 export type PrimopId = keyof Primops;
 type Primops = typeof primops
 type Primop = (...args: AbstractResult[]) => AbstractResult
 
+export const mathFloorPrimop = createUnaryPrimop('numbers', numberValue, Math.floor);
 export const primops = {
     'Math.floor': mathFloorPrimop as Primop
 }
 
-export function mathFloorPrimop(n: AbstractResult) {
-    return resultBind<number>(n, 'numbers', n => numberValue(Math.floor(n)));
+function createUnaryPrimop<A, R>(key: LatticeKey, construct: (val: R) => AbstractValue, f: (item: A) => R): Primop {
+    return (res: AbstractResult) => resultBind<A>(res, key, item => construct(f(item)));
 }
+
 
 export const primopMath = objectResult(
     ts.factory.createObjectLiteralExpression(), // dummy,
