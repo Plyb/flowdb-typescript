9b9326c9183ac60de1d778ef90b653419e037960
plyb <kobyjlewis@gmail.com>
2025-03-28 18:19:33 +0000

    remove abstract-results.ts

--- src/abstract-results.ts
+++
@@ -1,7 +0,0 @@
-import ts from 'typescript';
-import { AbstractValue, botValue, isTop, joinValue, nodeValue, topValue, NodeLattice } from './abstract-values';
-import { SimpleSet } from 'typescript-super-set';
-import { unimplementedVal } from './util';
-import { FixedEval } from './primops';
-import { getBuiltInMethod, getBuiltInValueOfBuiltInConstructor, getProtoOf, isBuiltInConstructorShaped, resultOfPropertyAccess } from './value-constructors';
-import { setSome } from './setUtil';
